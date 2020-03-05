8521ee8978aa0a97caf2ad8654b89101fa0d1e7b
MarvMarv <46840633+MarviniDeveloper@users.noreply.github.com>
2020-03-05 18:11:52 +0100

    init c#-Project

---
+++ tcp-client-server/tcp-client-server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tcp_client_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
